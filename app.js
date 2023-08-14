3af16a7b3115c83568999bb16485e0c3921bc51d
nikhilgla <nikhil.baghel.32@gmail.com>
2023-08-14 14:11:43 +0530

    webpage 4000 updated

--- app.js
+++ app.js
@@ -2,6 +2,35 @@ const http = require('http')
 
 const server =http.createServer((req,res) =>{
     console.log('Nikhil')
+    const url = req.url;
+    if(url === '/'){
+        res.write('<html>')
+        res.write('<head><title>Main page</title></head>')
+        res.write('<body><h1>Hello this is MAin PAge</h1></body>')
+        res.write('</html>')
+        res.end()
+    }
+    if(url === '/home'){
+        res.write('<html>')
+        res.write('<head><title>Home page</title></head>')
+        res.write('<body><h1>Welcome home</h1></body>')
+        res.write('</html>')
+        res.end()
+    }
+    if(url === '/about'){
+        res.write('<html>')
+        res.write('<head><title>about page</title></head>')
+        res.write('<body><h1>Welcome to About Us page</h1></body>')
+        res.write('</html>')
+        res.end()
+    }
+    if(url === '/node'){
+        res.write('<html>')
+        res.write('<head><title>node page</title></head>')
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+        res.write('</html>')
+        res.end()
+    }
 })
 
 server.listen(4000)
