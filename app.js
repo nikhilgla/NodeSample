b5fe328b931748a3007052d4a0b9b610c18133d0
nikhilgla <nikhil.baghel.32@gmail.com>
2023-08-16 10:57:01 +0530

    read write

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 const http = require('http')
+const fs =require('fs');
 
 const server =http.createServer((req,res) =>{
     console.log('Nikhil')
     const url = req.url;
+    const method = req.method;
+    var msg ;
     if(url === '/'){
         res.write('<html>')
         res.write('<head><title>Main page</title></head>')
-        res.write('<body><h1>Hello this is MAin PAge</h1></body>')
+        res.write(`<body><h1>${msg}</h1><br><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`)
         res.write('</html>')
-        res.end()
+        return res.end()
     }
-    if(url === '/home'){
-        res.write('<html>')
-        res.write('<head><title>Home page</title></head>')
-        res.write('<body><h1>Welcome home</h1></body>')
-        res.write('</html>')
-        res.end()
+    if(url === '/message' && method ==='POST' ){
+        const body =[]
+        req.on('data' , (chunk) => {
+            console.log(chunk);
+            body.push(chunk)
+        })
+        req.on('end' ,()=>{
+            const parsedBody =Buffer.concat(body).toString();
+            const message =parsedBody.split('=')[1];
+            msg= message;
+            fs.writeFileSync('message.txt' , message)
+            res.statusCode = 302;
+            res.setHeader('Location', '/' )
+            return res.end()
+            
+        })
     }
     if(url === '/about'){
         res.write('<html>')
