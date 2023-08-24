6e929f37547f2903552e4ecf772a088ae0b80c4b
nikhilgla <nikhil.baghel.32@gmail.com>
2023-08-24 14:44:50 +0530

    simple group chat

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const app = express();
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const loginRoutes = require('./routes/login') 
 
 app.use(bodyparser.urlencoded({extended:false}))
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
+app.use(loginRoutes);
 
 app.use((req,res,next)=>{
     res.status(404).send('<h1>Page not found</h1>')
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const fs = require('fs');
 
 const router = express.Router();
 
+const login = require('./login');
+
 router.get('/',(req,res,next)=>{
-    // console.log('in the middlewaresss');
-    res.send('<h1> hello to node js </h1>')
+    console.log('in the middlewaresss');
+    var data = fs.readFileSync('message.txt');
+    console.log(data.toString());
+    res.send(`<h1>hello</h1><h2>${data.toString()}</h2><form  onsubmit="document.getElementById('userId').value = localStorage.getItem('username')" action="/" method="POST"><input type="text" name="info" id="info"><input type="hidden" name="username" id="userId"><button type="submit">Add</button></form>`)
 });
+
+router.post('/', (req,res,next)=>{
+    console.log(req.body);
+    fs.appendFileSync('message.txt' , req.body.username +":"+ req.body.info+" " )
+    res.redirect('/');
+})
 
 module.exports = router;
